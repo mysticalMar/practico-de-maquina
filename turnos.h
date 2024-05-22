05ec6bbcf386141aa4bf3d72aae4cb9062973605
mysticalMar <150198113+mysticalMar@users.noreply.github.com>
2024-05-22 14:25:06 -0300

    Add files via upload

--- turnos.h
+++ turnos.h
@@ -2,10 +2,12 @@
 #define TURNOS_H_INCLUDED
 #define MAX 100
 typedef struct {
-int dia;
+int ndia;
+int hora[4];
+} dia;
+typedef struct {
+dia d[MAX];
 int mes;
-int hora;
-int estado; //0 ocupado 1 libre
 } fecha;
 typedef struct{
 char cliente[50];
@@ -16,7 +18,7 @@ int fpago;
 float subtotal;
 } turno;
 void precarga_fechas(turno f[], int d){
-    int i;
+    int i, o;
 FILE *fp=fopen("precarga.txt", "r");
  if(fopen("precarga.txt", "r")==NULL){
     printf("no es posible abrir el archivo \n");
@@ -24,34 +26,26 @@ FILE *fp=fopen("precarga.txt", "r");
  }
     while (!feof(fp)){
         for (i=0; i<d; i++){
-        fscanf(fp, "%d \n", &f[i].fturno[i].dia);
+        fscanf(fp, "%d \n", &f[i].fturno[i].d[i].ndia);
         fscanf(fp, "%d \n", &f[i].fturno[i].mes);
-        fscanf(fp, "%d \n", &f[i].fturno[i].hora);
-        fscanf(fp, "%d \n", &f[i].fturno[i].estado);
+            for (o=0; o<4; o++){
+            fscanf(fp, "%d \n", &f[i].fturno[i].d[i].hora[o]);
+            }
         }
     }
     fclose(fp);
 }
-int buscadia(turno f[], int dia, int mes, int d){
-    if (d<0){
-        return -1;
-    }
-    else {
-        if (f[d-1].fturno[d-1].dia==dia&&f[d-1].fturno[d-1].mes==mes){
-            return d-1;
-            exit(1);
-        }
-        else buscadia(f, dia, mes, d-1);
-    }
-}
+
 void muestrafechas(turno t[], int d){
-int i;
-for (i=0; i<d; i++){
-    printf("dia:%d \t", t[i].fturno[i].dia);
-    printf("mes:%d \t", t[i].fturno[i].mes);
-    printf("hora:%d \t", t[i].fturno[i].hora);
-    printf("estado: %d\n", t[i].fturno[i].estado);
-}
+    int i, o;
+        for (i=0; i<d; i++){
+            printf("dia: %d \t", t[i].fturno[i].d[i].ndia);
+            printf("mes: %d \n", t[i].fturno[i].mes);
+            for (o=0; o<4; o++){
+            printf("hora: %d \t", o);
+            printf("estado: %d \n", t[i].fturno[i].d[i].hora[o]);
+            }
+        }
 }
 
 #endif // TURNOS_H_INCLUDED
